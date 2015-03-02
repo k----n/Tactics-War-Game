ebb41da1d98410d1e6fee6eea7086152f0488944
k-----n <kalvin@kalvineng.com>
2015-03-01 17:45:47 -0700

    Initial Files

---
+++ unit/__init__.py
@@ -0,0 +1,16 @@
+__all__ = ['base_unit',
+           'ground_unit',
+           'water_unit',
+           'air_unit',
+           'tank',
+           'jeep',
+           'superjeep',
+           'anti_armour',
+           'battleship',
+           'carrier',
+           'artillery',
+           'fighter',
+           'anti_air',
+           'bomber',
+           'transport']
+unit_types = {}
